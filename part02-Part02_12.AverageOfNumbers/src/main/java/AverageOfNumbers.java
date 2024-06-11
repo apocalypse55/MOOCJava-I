89f7cc320672403c39ceb5432ffc78db2a6d3af4
apocalypse55 <shardool22rtipathi@gmail.com>
2024-06-11 19:03:58 +0545

    Part 2

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,20 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int totalNumbers = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            } else {
+                sum = sum + number;
+                totalNumbers = totalNumbers + 1;
+            }
+        }
+        double average = (double)sum/(double)totalNumbers;
+        System.out.println("Average of the numbers: " + average);
 
     }
 }
--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int totalPositiveNumbers = 0;
+        int sumOfpositiveNumbers = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+                
+            }
+            if (number > 0) {
+                totalPositiveNumbers = totalPositiveNumbers + 1;
+                sumOfpositiveNumbers = sumOfpositiveNumbers + number;
+            }
+            continue;
+            
+        }
+        if (totalPositiveNumbers == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double average = (double) sumOfpositiveNumbers / (double) totalPositiveNumbers;
+            System.out.println(average);
+        }
     }
 }
