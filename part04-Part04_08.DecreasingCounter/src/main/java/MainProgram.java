d31ad7a9c3138fd2ead4f38a7dc2d42d1d3ae068
apocalypse55 <shardool22rtipathi@gmail.com>
2024-09-22 13:46:05 +0545

    Updated part 4 exercise 16

--- part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -2,32 +2,13 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(10);
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-
-        paulsCard.eatHeartily();
-        System.out.println("Paul: " + paulsCard);
-        mattsCard.eatAffordably();
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.addMoney(20);
-        System.out.println("Paul: " + paulsCard);
-        mattsCard.eatHeartily();
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        System.out.println("Paul: " + paulsCard);
-        mattsCard.addMoney(50);
-        System.out.println("Matt: " + mattsCard);
-
-        // DecreasingCounter counter = new DecreasingCounter(10);
-
-        // counter.printValue();
-        // counter.decrement();
-        // counter.decrement();
-        // counter.printValue();
-        // counter.reset();
-        // counter.printValue();
+        DecreasingCounter counter = new DecreasingCounter(10);
+        counter.printValue();
+        counter.decrement();
+        counter.decrement();
+        counter.printValue();
+        counter.reset();
+        counter.printValue();
 
         // System.out.println(card);
         // card.eatHeartily();
